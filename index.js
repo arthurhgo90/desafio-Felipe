e5d47f12cab6d5c478a7b80837d968d0a0d56b52
arthurhgo90 <arthur_hgo@hotmail.com>
2026-01-12 02:26:02 -0300

    Desafio finalizado

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log('oi');
+let nome = "Arthur";
+let quantExp = 5000;
+let nivel = "";
+
+if (quantExp <= 1000){
+    nivel = 'Ferro';
+}else if (quantExp <= 2000){
+    nivel = 'Bronze';
+}else if (quantExp <= 5000){
+    nivel = 'Prata';
+}else if (quantExp <= 7000){
+    nivel = 'Ouro';
+}else if (quantExp <= 8000){
+    nivel = 'Platina';
+}else if (quantExp <= 9000){
+    nivel = 'Ascendente';
+}else if (quantExp <= 10000){
+    nivel = 'Imortal';
+}else{
+    nivel = 'Radiante';
+}
+
+console.log("O Herói de nome " + nome + " está no nível de " + nivel);
+
